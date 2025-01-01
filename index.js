c0b654bf7f6392654ef7c331e8dcfafc6d38ca0d
EuniceTheWeeb - <eunicethung@yahoo.com>
2025-01-01 17:19:11 +0000

    create & read work

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { createConnection } = require('mysql2/promise');
 let app = express();
 app.set('view engine', 'hbs');
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
 wax.on(hbs.handlebars);
 wax.setLayoutPath('./views/layouts');
@@ -22,13 +22,48 @@ async function main() {
         'password': process.env.DB_PASSWORD
     })
 
-    app.get('/', (req,res) => {
+    app.get('/', (req, res) => {
         res.send('Hello, World!');
     });
 
-    app.listen(3000, ()=>{
-        console.log('Server is running')
-    });
+// MARK: read
+    app.get('/customers', async (req, res) => {
+        const [customers] = await connection.execute({
+            'sql': `
+            SELECT * from Customers
+                JOIN Companies ON Customers.company_id = Companies.company_id;
+            `,
+            nestTables: true
+        });
+        res.render('customers/index', {
+            'customers': customers
+        })
+    })
+
+    // MARK: create
+    app.get('/customers/create', async(req,res)=>{
+        let [companies] = await connection.execute('SELECT * from Companies');
+        res.render('customers/create', {
+            'companies': companies
+        })
+    })
+
+    app.post('/customers/create', async(req,res)=>{
+        let {first_name, last_name, rating, company_id} = req.body;
+        let query = 'INSERT INTO Customers (first_name, last_name, rating, company_id) VALUES (?, ?, ?, ?)';
+        let bindings = [first_name, last_name, rating, company_id];
+        await connection.execute(query, bindings);
+        res.redirect('/customers');
+    })
+    
+    app.get('/employees', (req, res) => {
+        res.render('employees/index');
+      });
 }
 
 main();
+
+// MARK: Always last
+app.listen(3000, () => {
+    console.log('Server is running')
+});
